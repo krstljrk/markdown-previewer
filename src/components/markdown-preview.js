b3d9581aecc266f519e0ceb3f6adfb2eb87c242d
krstjrk <kristel.juurik@gmail.com>
2021-09-20 17:58:35 +0300

    changes regards handling state locally

--- src/components/markdown-preview.js
+++ src/components/markdown-preview.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/stylesheet.scss';
 import { useSelector } from 'react-redux';
 
 export default function MarkdownPreviewer() {
-    const state = useSelector(state => state.markdown)
+    const [input, setInput] = useState('');
+
+    const handleChange = (event) => {
+        setInput(event.target.value)
+    }
 
     return (
         <div className="markdown-container">
-            <textarea id="editor" />
+            <textarea id="editor" value={input} onChange={handleChange} />
             <div id="preview">
-                {state.markdownInput}
+                {input}
             </div>
         </div>
     )
