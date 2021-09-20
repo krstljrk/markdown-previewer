650fa8bc0f19b570a10a9ce7f0ebc599296b0792
krstjrk <kristel.juurik@gmail.com>
2021-09-20 18:59:50 +0300

    added basic styling to app

--- src/components/markdown-preview.js
+++ src/components/markdown-preview.js
@@ -10,11 +10,21 @@ export default function MarkdownPreviewer() {
     }
 
     return (
-        <div className="markdown-container">
-            <textarea id="editor" value={input} onChange={handleChange} />
-            <div id="preview">
-                {input}
+        <div className="markdown-container container-fluid">
+            <div className="row">
+                <div className="col-5 container rounded">
+                    <h4 className="text-center">Enter your Markdown:</h4>
+                    <textarea className="form-control" id="editor" value={input} onChange={handleChange} />
+                </div>
+                <div className="col-5 container rounded">
+                    <h4 className="text-center">Preview text:</h4>
+                    <div id="preview">
+                        {input}
+                    </div>
+                </div>
             </div>
+
+
         </div>
     )
 }
