a9b5a4fa1288c7d4ef2a7fb3e407a6f753b2d879
krstjrk <kristel.juurik@gmail.com>
2021-09-20 17:59:18 +0300

    changes regard handling state locally

--- src/index.js
+++ src/index.js
@@ -3,20 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux';
-import markdownReducer from './features/markdown';
 
-const store = configureStore({
-  reducer: {
-    markdown: markdownReducer
-  }
-});
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-    
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
