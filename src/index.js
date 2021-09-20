cd6793aefe0c91777e4e82634bc1c0f69e940c1a
krstjrk <kristel.juurik@gmail.com>
2021-09-20 17:25:16 +0300

    import reducer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux';
+import markdownReducer from './features/markdown';
+
+const store = configureStore({
+  reducer: {
+    markdown: markdownReducer
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
